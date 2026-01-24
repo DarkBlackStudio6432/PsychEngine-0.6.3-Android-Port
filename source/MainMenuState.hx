b3660f2f959edb8182fc60fcd923bfb0274afc60
DarkBlackStudio6432 <95198141+DarkBlackStudio6432@users.noreply.github.com>
2026-01-24 00:18:16 -0300

    Atualizar o MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -10,22 +10,15 @@ import flixel.FlxSprite;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
-import flixel.tweens.FlxTween;
 import flixel.effects.FlxFlicker;
-
-import lime.app.Application;
+import flixel.tweens.FlxTween;
 
 import options.OptionsState;
-
-#if mobile
-import mobile.MobilePad;
-#end
 
 using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-    // OBRIGATÓRIO NA 0.6.3
     public static var psychEngineVersion:String = "0.6.3";
     public static var curSelected:Int = 0;
 
@@ -40,10 +33,6 @@ class MainMenuState extends MusicBeatState
         'options'
     ];
 
-    #if mobile
-    var mobilePad:MobilePad;
-    #end
-
     override function create()
     {
         #if desktop
@@ -52,7 +41,6 @@ class MainMenuState extends MusicBeatState
 
         persistentUpdate = persistentDraw = true;
 
-        // FUNDO
         var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuBG'));
         bg.screenCenter();
         bg.antialiasing = ClientPrefs.data.antialiasing;
@@ -66,32 +54,27 @@ class MainMenuState extends MusicBeatState
 
         for (i in 0...optionShit.length)
         {
-            var offset:Float = 200;
-            var menuItem:FlxSprite = new FlxSprite(0, offset + (i * 140));
-            menuItem.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionShit[i]);
-            menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
-            menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
-            menuItem.animation.play('idle');
-            menuItem.ID = i;
-            menuItem.antialiasing = ClientPrefs.data.antialiasing;
-            menuItem.screenCenter(X);
-            menuItems.add(menuItem);
+            var item:FlxSprite = new FlxSprite(0, 200 + i * 140);
+            item.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionShit[i]);
+            item.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
+            item.animation.addByPrefix('selected', optionShit[i] + " white", 24);
+            item.animation.play('idle');
+            item.ID = i;
+            item.antialiasing = ClientPrefs.data.antialiasing;
+            item.screenCenter(X);
+            menuItems.add(item);
         }
 
         changeItem();
-
         FlxG.camera.follow(camFollow, null, 9);
 
-        // VERSÃO NO CANTO
         var verText:FlxText = new FlxText(12, FlxG.height - 24, 0,
             "Psych Engine v" + psychEngineVersion, 16);
         verText.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, OUTLINE, FlxColor.BLACK);
         add(verText);
 
         #if mobile
-        mobilePad = new MobilePad(UP_DOWN, A_B);
-        add(mobilePad);
-        controls.setVirtualPad(mobilePad);
+        addVirtualPad(UP_DOWN, A_B);
         #end
 
         super.create();
@@ -101,11 +84,8 @@ class MainMenuState extends MusicBeatState
     {
         if (!selectedSomethin)
         {
-            if (controls.UI_UP_P)
-                changeItem(-1);
-
-            if (controls.UI_DOWN_P)
-                changeItem(1);
+            if (controls.UI_UP_P) changeItem(-1);
+            if (controls.UI_DOWN_P) changeItem(1);
 
             if (controls.BACK)
             {
@@ -127,11 +107,8 @@ class MainMenuState extends MusicBeatState
         menuItems.members[curSelected].animation.play('idle');
 
         curSelected += huh;
-
-        if (curSelected < 0)
-            curSelected = menuItems.length - 1;
-        if (curSelected >= menuItems.length)
-            curSelected = 0;
+        if (curSelected < 0) curSelected = menuItems.length - 1;
+        if (curSelected >= menuItems.length) curSelected = 0;
 
         var item = menuItems.members[curSelected];
         item.animation.play('selected');
@@ -157,13 +134,10 @@ class MainMenuState extends MusicBeatState
             {
                 case 'story_mode':
                     MusicBeatState.switchState(new StoryMenuState());
-
                 case 'freeplay':
                     MusicBeatState.switchState(new FreeplayState());
-
                 case 'credits':
                     MusicBeatState.switchState(new CreditsState());
-
                 case 'options':
                     MusicBeatState.switchState(new OptionsState());
             }
