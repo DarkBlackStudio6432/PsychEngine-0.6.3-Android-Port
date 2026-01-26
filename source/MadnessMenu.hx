1d6c9b840793a1d38ec5d51b2c033e730383a11f
DarkBlackStudio6432 <95198141+DarkBlackStudio6432@users.noreply.github.com>
2026-01-26 18:49:22 -0300

    Atualizar o MadnessMenu.hx

--- source/MadnessMenu.hx
+++ source/MadnessMenu.hx
@@ -15,116 +15,115 @@ import flixel.addons.display.FlxBackdrop;
 
 enum Hovering
 {
-	ANYTHINGELSE;
+        ANYTHINGELSE;
 }
 
 class MadnessMenu extends MusicBeatState
 {
-	var uniScale:Float = 1;
-	var currentSel:Int = 0;
+        var uniScale:Float = 1;
+        var currentSel:Int = 0;
 
-	var baseButtons:FlxTypedGroup<FlxSprite>;
-	var circles:FlxSpriteGroup;
-	var storyButton:FlxSprite;
+        var baseButtons:FlxTypedGroup<FlxSprite>;
+        var circles:FlxSpriteGroup;
+        var storyButton:FlxSprite;
 
-	override function create()
-	{
-		FlxG.camera.antialiasing = ClientPrefs.antialiasing;
-		persistentUpdate = true;
+        override function create()
+        {       
+                persistentUpdate = true;
 
-		var back = new FlxSprite(Paths.image('madnessmenu/back'));
-		back.setGraphicSize(FlxG.width);
-		back.updateHitbox();
-		back.screenCenter(Y);
-		back.y += 100;
-		add(back);
+                var back = new FlxSprite(Paths.image('madnessmenu/back'));
+                back.setGraphicSize(FlxG.width);
+                back.updateHitbox();
+                back.screenCenter(Y);
+                back.y += 100;
+                add(back);
 
-		uniScale = back.scale.x;
+                uniScale = back.scale.x;
 
-		var silh = new FlxBackdrop(
-			Paths.image('madnessmenu/siloets'),
-			0,
-			20
-		);
-		silh.scale.set(uniScale, uniScale);
-		silh.y = 300;
-		silh.velocity.x = -50;
-		silh.alpha = 0.3;
-		add(silh);
+                var silh = new FlxBackdrop(
+                        Paths.image('madnessmenu/siloets'),
+                        0,
+                        20
+                );
+                silh.scale.set(uniScale, uniScale);
+                silh.y = 300;
+                silh.velocity.x = -50;
+                silh.alpha = 0.3;
+                add(silh);
 
-		baseButtons = new FlxTypedGroup<FlxSprite>();
-		add(baseButtons);
+                baseButtons = new FlxTypedGroup<FlxSprite>();
+                add(baseButtons);
 
-		storyButton = makeButton('storymode');
-		storyButton.setPosition(1169 * uniScale, 405 * uniScale);
-		baseButtons.add(storyButton);
+                storyButton = makeButton('storymode');
+                storyButton.setPosition(1169 * uniScale, 405 * uniScale);
+                baseButtons.add(storyButton);
 
-		var freeplayButton = makeButton('freeplay');
-		freeplayButton.setPosition(
-			storyButton.x + storyButton.width + 10,
-			storyButton.y
-		);
-		baseButtons.add(freeplayButton);
+                var freeplayButton = makeButton('freeplay');
+                freeplayButton.setPosition(
+                        storyButton.x + storyButton.width + 10,
+                        storyButton.y
+                );
+                baseButtons.add(freeplayButton);
 
-		circles = new FlxSpriteGroup();
-		add(circles);
+                circles = new FlxSpriteGroup();
+                add(circles);
 
-		super.create();
-		changeSel();
-	}
+                super.create();
+                changeSel();
+        }
 
-	override function update(elapsed:Float)
-	{
-		super.update(elapsed);
+        override function update(elapsed:Float)
+        {
+                super.update(elapsed);
 
-		if (controls.UI_LEFT_P || controls.UI_RIGHT_P)
-			changeSel(controls.UI_LEFT_P ? -1 : 1);
+                if (controls.UI_LEFT_P || controls.UI_RIGHT_P)
+                        changeSel(controls.UI_LEFT_P ? -1 : 1);
 
-		if (controls.ACCEPT)
-			confirmSel();
-	}
+                if (controls.ACCEPT)
+                        confirmSel();
+        }
 
-	function confirmSel()
-	{
-		FlxG.sound.play(Paths.sound('madness/select'));
+        function confirmSel()
+        {
+                FlxG.sound.play(Paths.sound('madness/select'));
 
-		var button = baseButtons.members[currentSel];
-		button.animation.play('confirm');
+                var button = baseButtons.members[currentSel];
+                button.animation.play('confirm');
 
-		switch (currentSel)
-		{
-			case 0:
-				MusicBeatState.switchState(new StoryMenuState());
-			case 1:
-				MusicBeatState.switchState(new CreditsState());
-		}
-	}
+                switch (currentSel)
+                {
+                        case 0:
+                                MusicBeatState.switchState(new StoryMenuState());
+                        case 1:
+                                MusicBeatState.switchState(new CreditsState());
+                }
+        }
 
-	function changeSel(v:Int = 0)
-	{
-		FlxG.sound.play(Paths.sound('madness/beep'));
+        function changeSel(v:Int = 0)
+        {
+                FlxG.sound.play(Paths.sound('madness/beep'));
 
-		for (i in baseButtons.members)
-			i.animation.play('i');
+                for (i in baseButtons.members)
+                        i.animation.play('i');
 
-		currentSel = FlxMath.wrap(
-			currentSel + v,
-			0,
-			baseButtons.length - 1
-		);
+                currentSel = FlxMath.wrap(
+                        currentSel + v,
+                        0,
+                        baseButtons.length - 1
+                );
 
-		baseButtons.members[currentSel].animation.play('select');
-	}
+                baseButtons.members[currentSel].animation.play('select');
+        }
 
-	function makeButton(path:String):FlxSprite
-	{
-		var spr = new FlxSprite();
-		spr.frames = Paths.getSparrowAtlas('madnessmenu/' + path);
-		spr.animation.addByPrefix('i', path + '0');
-		spr.animation.addByPrefix('confirm', path + ' confirm');
-		spr.animation.addByPrefix('select', path + ' select');
-		spr.animation.play('i');
-		spr.scale.set(uniScale + 0.2, uniScale + 0.2);
-		return spr;
-	}
+        function makeButton(path:String):FlxSprite
+        {
+                var spr = new FlxSprite();
+                spr.frames = Paths.getSparrowAtlas('madnessmenu/' + path);
+                spr.animation.addByPrefix('i', path + '0');
+                spr.animation.addByPrefix('confirm', path + ' confirm');
+                spr.animation.addByPrefix('select', path + ' select');
+                spr.animation.play('i');
+                spr.scale.set(uniScale + 0.2, uniScale + 0.2);
+                return spr;
+        }
 }
