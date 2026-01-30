4e31ff50ad6ada07d16b8e9d7d4de0abc2487cd1
DarkBlackStudio6432 <95198141+DarkBlackStudio6432@users.noreply.github.com>
2026-01-30 19:33:05 -0300

    Atualizar o OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -3,7 +3,6 @@ package options;
 import objects.AttachedSprite;
 import backend.MadnessTransition;
 import flixel.FlxSubState;
-import AttachedSprite;
 import states.MadnessMenu;
 import backend.StageData;
 import flixel.FlxG;
