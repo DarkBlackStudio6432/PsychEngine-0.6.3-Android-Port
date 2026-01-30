7a33651fb486d9ac0097b82593719a4a97112fba
DarkBlackStudio6432 <95198141+DarkBlackStudio6432@users.noreply.github.com>
2026-01-30 16:34:16 -0300

    Atualizar o HealthIcon.hx

--- source/objects/HealthIcon.hx
+++ source/objects/HealthIcon.hx
@@ -1,61 +1,78 @@
 package objects;
 
+import flixel.FlxSprite;
+import backend.Paths;
+import backend.ClientPrefs;
+
 class HealthIcon extends FlxSprite
 {
-	public var sprTracker:FlxSprite;
-	private var isOldIcon:Bool = false;
-	private var isPlayer:Bool = false;
-	private var char:String = '';
+    public var sprTracker:FlxSprite;
+    private var isOldIcon:Bool = false;
+    private var isPlayer:Bool = false;
+    private var char:String = '';
 
-	public function new(char:String = 'bf', isPlayer:Bool = false, ?allowGPU:Bool = true)
-	{
-		super();
-		isOldIcon = (char == 'bf-old');
-		this.isPlayer = isPlayer;
-		changeIcon(char, allowGPU);
-		scrollFactor.set();
-	}
+    private var iconOffsets:Array<Float> = [0, 0];
 
-	override function update(elapsed:Float)
-	{
-		super.update(elapsed);
+    public function new(char:String = "bf", isPlayer:Bool = false, ?allowGPU:Bool = true)
+    {
+        super();
+        isOldIcon = (char == "bf-old");
+        this.isPlayer = isPlayer;
 
-		if (sprTracker != null)
-			setPosition(sprTracker.x + sprTracker.width + 12, sprTracker.y - 30);
-	}
+        changeIcon(char, allowGPU);
+        scrollFactor.set(); // sem parâmetros, fixo
+    }
 
-	private var iconOffsets:Array<Float> = [0, 0];
-	public function changeIcon(char:String, ?allowGPU:Bool = true) {
-		if(this.char != char) {
-			var name:String = 'icons/' + char;
-			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char; //Older versions of psych engine's support
-			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
-			
-			var graphic = Paths.image(name, allowGPU);
-			loadGraphic(graphic, true, Math.floor(graphic.width / 2), Math.floor(graphic.height));
-			iconOffsets[0] = (width - 150) / 2;
-			iconOffsets[1] = (height - 150) / 2;
-			updateHitbox();
+    override function update(elapsed:Float)
+    {
+        super.update(elapsed);
 
-			animation.add(char, [0, 1], 0, false, isPlayer);
-			animation.play(char);
-			this.char = char;
+        if(sprTracker != null)
+            setPosition(sprTracker.x + sprTracker.width + 12, sprTracker.y - 30);
+    }
 
-			if(char.endsWith('-pixel'))
-				antialiasing = false;
-			else
-				antialiasing = ClientPrefs.data.antialiasing;
-		}
-	}
+    public function changeIcon(char:String, ?allowGPU:Bool = true)
+    {
+        if(this.char != char)
+        {
+            var name:String = "icons/" + char;
 
-	override function updateHitbox()
-	{
-		super.updateHitbox();
-		offset.x = iconOffsets[0];
-		offset.y = iconOffsets[1];
-	}
+            // fallback caso o arquivo não exista
+            if(!Paths.fileExists("images/" + name + ".png", IMAGE))
+                name = "icons/icon-" + char;
 
-	public function getCharacter():String {
-		return char;
-	}
+            if(!Paths.fileExists("images/" + name + ".png", IMAGE))
+                name = "icons/icon-face"; // último fallback
+
+            // Carregar a imagem via Paths
+            var graphic = Paths.image(name, allowGPU);
+            loadGraphic(graphic, true, Math.floor(graphic.width / 2), Math.floor(graphic.height));
+
+            iconOffsets[0] = (width - 150) / 2;
+            iconOffsets[1] = (height - 150) / 2;
+
+            updateHitbox();
+
+            // animação simples
+            animation.add(char, [0, 1], 0, false, isPlayer);
+            animation.play(char);
+
+            this.char = char;
+
+            // antialiasing
+            antialiasing = char.endsWith("-pixel") ? false : ClientPrefs.data.antialiasing;
+        }
+    }
+
+    override function updateHitbox()
+    {
+        super.updateHitbox();
+        offset.x = iconOffsets[0];
+        offset.y = iconOffsets[1];
+    }
+
+    public function getCharacter():String
+    {
+        return char;
+    }
 }
