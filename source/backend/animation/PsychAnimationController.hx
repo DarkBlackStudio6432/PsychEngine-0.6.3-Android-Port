2f352e526f242d10e07a11d27c2ac550c2f4f525
DarkBlackStudio6432 <95198141+DarkBlackStudio6432@users.noreply.github.com>
2026-01-30 16:00:43 -0300

    Atualizar o PsychAnimationController.hx

--- source/backend/animation/PsychAnimationController.hx
+++ source/backend/animation/PsychAnimationController.hx
@@ -1,18 +1,22 @@
 package backend.animation;
 
 import flixel.animation.FlxAnimationController;
+import flixel.FlxG;
 
 class PsychAnimationController extends FlxAnimationController {
-    public var followGlobalSpeed:Bool = true;
+
+    // Usaremos uma variável própria para controlar se quer multiplicar pelo global speed
+    public var useGlobalSpeed:Bool = true;
 
     public override function update(elapsed:Float):Void {
-		if (_curAnim != null) {
+        if (_curAnim != null) {
             var speed:Float = timeScale;
-            if (followGlobalSpeed) speed *= FlxG.animationTimeScale;
-			_curAnim.update(elapsed * speed);
-		}
-		else if (_prerotated != null) {
-			_prerotated.angle = _sprite.angle;
-		}
-	}
+            // Multiplica pelo FlxG.animationTimeScale apenas se nossa flag estiver ativa
+            if (useGlobalSpeed) speed *= FlxG.animationTimeScale;
+            _curAnim.update(elapsed * speed);
+        }
+        else if (_prerotated != null) {
+            _prerotated.angle = _sprite.angle;
+        }
+    }
 }
