d508393d3bee9e95a0f3cdb9ac7c78cd3f9f9d4f
DarkBlackStudio6432 <95198141+DarkBlackStudio6432@users.noreply.github.com>
2026-01-30 19:13:49 -0300

    Atualizar o import.hx

--- source/import.hx
+++ source/import.hx
@@ -1,9 +1,11 @@
 #if !macro
+// Core backend
 import backend.Paths;
 import backend.CoolUtil;
 import backend.Conductor;
 import backend.Highscore;
-//import states.TitleState;
+
+// Sys / JS
 #if sys
 import sys.*;
 import sys.io.*;
@@ -11,47 +13,7 @@ import sys.io.*;
 import js.html.*;
 #end
 
-// Mobile Things
-#if TOUCH_CONTROLS
-import mobile.flixel.*;
-import mobile.states.*;
-import mobile.objects.*;
-import mobile.options.*;
-import mobile.backend.*;
-import mobile.psychlua.*;
-import mobile.substates.*;
-import mobile.objects.Hitbox;
-import mobile.objects.MobilePad;
-import mobile.backend.MobileData;
-#else
-import mobile.backend.StorageUtil;
-import mobile.backend.PsychJNI;
-import mobile.options.*;
-import mobile.backend.MobileScaleMode;
-#end
-
-// Android
-#if android
-import android.Tools as AndroidTools;
-import android.Settings as AndroidSettings;
-import android.widget.Toast as AndroidToast;
-import android.content.Context as AndroidContext;
-import android.Permissions as AndroidPermissions;
-import android.os.Build.VERSION as AndroidVersion;
-import android.os.Environment as AndroidEnvironment;
-import android.os.BatteryManager as AndroidBatteryManager;
-import android.os.Build.VERSION_CODES as AndroidVersionCode;
-#end
-
-// Lua
-#if LUA_ALLOWED
-import llua.Lua;
-import llua.LuaL;
-import llua.State;
-import llua.Convert;
-#end
-
-//Flixel
+// Flixel base (ok ser global)
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxCamera;
