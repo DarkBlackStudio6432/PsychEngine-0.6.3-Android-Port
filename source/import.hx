84025ed556ef92b30cfeb8d05572c63bed67889a
DarkBlackStudio6432 <95198141+DarkBlackStudio6432@users.noreply.github.com>
2026-01-31 19:21:09 -0300

    Atualizar o import.hx

--- source/import.hx
+++ source/import.hx
@@ -29,15 +29,9 @@ import mobile.backend.MobileScaleMode;
 
 // Android
 #if android
-import android.Tools as AndroidTools;
-import android.Settings as AndroidSettings;
-import android.widget.Toast as AndroidToast;
-import android.content.Context as AndroidContext;
-import android.Permissions as AndroidPermissions;
-import android.os.Build.VERSION as AndroidVersion;
-import android.os.Environment as AndroidEnvironment;
-import android.os.BatteryManager as AndroidBatteryManager;
-import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import extension.android.tools.AndroidTools;
+import extension.android.tools.AndroidSettings;
+import extension.android.tools.Toast;
 #end
 
 // Lua
