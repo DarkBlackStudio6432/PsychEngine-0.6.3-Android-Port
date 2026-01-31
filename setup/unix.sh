da3dc3232b9317651301a090c61ced97cd7ec830
DarkBlackStudio6432 <95198141+DarkBlackStudio6432@users.noreply.github.com>
2026-01-31 17:57:07 -0300

    Atualizar o unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -1,20 +1,33 @@
 #!/bin/sh
-# SETUP FOR MAC AND LINUX SYSTEMS!!!
-# REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
-# https://haxe.org/download
+set -e
+
 cd ..
-echo Makking the main haxelib and setuping folder in same time..
+
+echo Making the main haxelib and setuping folder..
 haxelib setup ~/haxelib
+
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
+
 haxelib git linc_luajit https://github.com/PsychExtendedThings/linc_luajit --quiet
 haxelib install tjson --quiet
+
 haxelib install flixel 5.2.2 --quiet
 haxelib install flixel-addons 2.11.0 --quiet
 haxelib install flixel-ui 2.4.0 --quiet
 haxelib install hscript 2.4.0 --quiet
+
 haxelib git hxCodec https://github.com/PsychExtendedThings/hxCodec-0.6.3 --quiet
 haxelib git hxcpp https://github.com/PsychExtendedThings/hxcpp --quiet
-haxelib git lime https://github.com/PsychExtendedThings/lime-new --quiet
+
+# 🔥 ISSO AQUI É O QUE FALTAVA
+haxelib install flxanimate --quiet
+
+# ⚠️ Lime + OpenFL compatíveis com Psych 0.6.3
+haxelib install lime 7.9.0 --quiet
 haxelib install openfl 9.2.2 --quiet
+
+haxelib run lime setup
+haxelib run openfl setup
+
 echo Finished!
