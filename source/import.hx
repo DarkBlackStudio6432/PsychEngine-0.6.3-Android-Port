8ee445c03b3254ee2a81f258528db82c18bce97b
DarkBlackStudio6432 <95198141+DarkBlackStudio6432@users.noreply.github.com>
2026-01-30 23:20:26 -0300

    Atualizar o import.hx

--- source/import.hx
+++ source/import.hx
@@ -1,3 +1,5 @@
+package;
+
 import backend.Paths;
 import backend.ClientPrefs;
 import backend.Conductor;
@@ -6,3 +8,11 @@ import backend.Highscore;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.util.FlxColor; // 🔹 Import necessário para FlxColor
+
+// Mobile Controls
+#if mobile
+import mobile.controls.MobileControls as Controls;
+#else
+import backend.Controls;
+#end
