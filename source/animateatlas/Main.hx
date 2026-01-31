3d96ab94cbd68f1ac096dade2e7d3b544b24ecb0
DarkBlackStudio6432 <95198141+DarkBlackStudio6432@users.noreply.github.com>
2026-01-30 22:27:23 -0300

    Atualizar o Main.hx

--- source/animateatlas/Main.hx
+++ source/animateatlas/Main.hx
@@ -1,109 +1,98 @@
-package;
+package animateatlas;
 
 import animateatlas.JSONData.AtlasData;
-import openfl.display.BitmapData;
 import animateatlas.JSONData.AnimationData;
-import openfl.display.FPS;
-import openfl.Lib;
-import openfl.events.MouseEvent;
 import animateatlas.HelperEnums.LoopMode;
-import openfl.events.Event;
-import openfl.display.Tilemap;
-import openfl.display.Tileset;
-import openfl.Assets;
-import haxe.Json;
 import animateatlas.tilecontainer.TileAnimationLibrary;
 import animateatlas.tilecontainer.TileContainerMovieClip;
 import animateatlas.displayobject.SpriteAnimationLibrary;
 import animateatlas.displayobject.SpriteMovieClip;
+
+import openfl.Lib;
+import openfl.display.BitmapData;
 import openfl.display.Sprite;
+import openfl.events.Event;
+import openfl.Assets;
+import haxe.Json;
+import Math;
 
-class Main extends Sprite {
-	var aa:TileAnimationLibrary;
-	var ss:SpriteAnimationLibrary;
+class AnimateAtlasTestAuto extends Sprite {
+    var aa:TileAnimationLibrary;
+    var ss:SpriteAnimationLibrary;
 
-	//
-	var tileSymbols:Array<TileContainerMovieClip>;
+    var tileSymbols:Array<TileContainerMovieClip>;
+    var spriteSymbols:Array<SpriteMovieClip>;
 
-	var spriteSymbols:Array<SpriteMovieClip>;
+    var renderer:Sprite;
 
-	var renderer:Tilemap;
+    public function new() {
+        super();
 
-	public function new() {
-		super();
-		graphics.beginFill(0x333333);
-		graphics.drawRect(0, 0, Lib.current.stage.stageWidth, Lib.current.stage.stageHeight);
+        // Fundo cinza
+        graphics.beginFill(0x333333);
+        graphics.drawRect(0, 0, Lib.current.stage.stageWidth, Lib.current.stage.stageHeight);
 
-		var animationData:AnimationData = Json.parse(Assets.getText("assets/TEST/Animation.json"));
-		var atlasData:AtlasData = Json.parse(Assets.getText("assets/TEST/spritemap.json"));
-		var bitmapData:BitmapData = Assets.getBitmapData("assets/TEST/spritemap.png");
+        // Carrega JSON e bitmap (substitua pelos seus assets)
+        var animationData:AnimationData = Json.parse(Assets.getText("assets/TEST/Animation.json"));
+        var atlasData:AtlasData = Json.parse(Assets.getText("assets/TEST/spritemap.json"));
+        var bitmapData:BitmapData = Assets.getBitmapData("assets/TEST/spritemap.png");
 
-		aa = new TileAnimationLibrary(animationData, atlasData, bitmapData);
-		ss = new SpriteAnimationLibrary(animationData, atlasData, bitmapData);
+        aa = new TileAnimationLibrary(animationData, atlasData, bitmapData);
+        ss = new SpriteAnimationLibrary(animationData, atlasData, bitmapData);
 
-		renderer = new Tilemap(Lib.current.stage.stageWidth, Lib.current.stage.stageHeight, null, true);
+        renderer = new Sprite();
+        addChild(renderer);
 
-		renderer.tileAlphaEnabled = false;
-		renderer.tileBlendModeEnabled = false;
-		renderer.tileColorTransformEnabled = false;
+        tileSymbols = [];
+        spriteSymbols = [];
 
-		addChild(renderer);
-		addChild(new FPS(10, 10, 0xFFFFFF));
+        // Começar spawn automático de animações
+        addEventListener(Event.ENTER_FRAME, update);
+        spawnTimer = 0;
+    }
 
-		tileSymbols = [];
-		spriteSymbols = [];
+    var prev:Int = 0;
+    var dt:Int = 0;
+    var curr:Int = 0;
 
-		addEventListener(Event.ENTER_FRAME, update);
-		// addEventListener(MouseEvent.CLICK, addSpriteGirl);
-		addEventListener(MouseEvent.CLICK, addTileGirl);
-	}
+    var spawnTimer:Float = 0;
+    var spawnInterval:Float = 500; // spawn a cada 500ms
 
-	var prev:Int = 0;
-	var dt:Int = 0;
-	var curr:Int = 0;
+    public function update(e:Event):Void {
+        curr = Lib.getTimer();
+        dt = curr - prev;
+        prev = curr;
 
-	public function update(_) {
-		// making a dt
-		curr = Lib.getTimer();
-		dt = curr - prev;
-		prev = curr;
+        spawnTimer += dt;
+        if (spawnTimer >= spawnInterval) {
+            spawnTileAnimation();
+            spawnSpriteAnimation();
+            spawnTimer = 0;
+        }
 
-		for (symbol in tileSymbols) {
-			symbol.update(dt);
-		}
-		for (symbol in spriteSymbols) {
-			symbol.update(dt);
-		}
-	}
+        for (symbol in tileSymbols)
+            symbol.update(dt);
+        for (symbol in spriteSymbols)
+            symbol.update(dt);
+    }
 
-	public function addSpriteGirl(_) {
-		for (i in 0...1) {
-			var t = ss.createAnimation();
-			t.x = mouseX + i * 20 * (-1 * i % 2);
-			t.y = mouseY + i * 20 * (-1 * i % 2);
+    function spawnTileAnimation():Void {
+        var t = aa.createAnimation();
+        t.x = Math.random() * Lib.current.stage.stageWidth;
+        t.y = Math.random() * Lib.current.stage.stageHeight;
 
-			addChild(t);
-			t.loopMode = LoopMode.SINGLE_FRAME;
+        renderer.addChild(t);
+        t.loopMode = LoopMode.LOOP;
+        tileSymbols.push(t);
+    }
 
-			t.currentLabel = t.getFrameLabels()[Std.random(t.getFrameLabels().length)];
-			spriteSymbols.push(t);
-			trace(spriteSymbols.length);
-		}
-	}
+    function spawnSpriteAnimation():Void {
+        var s = ss.createAnimation();
+        s.x = Math.random() * Lib.current.stage.stageWidth;
+        s.y = Math.random() * Lib.current.stage.stageHeight;
 
-	public function addTileGirl(_) {
-		for (i in 0...1) {
-			var t = aa.createAnimation();
-			t.x = mouseX + i * 5 * (-1 * i % 2);
-			t.y = mouseY + i * 5 * (-1 * i % 2);
-
-			renderer.addTile(t);
-			t.loopMode = LoopMode.SINGLE_FRAME;
-
-			t.currentLabel = t.getFrameLabels()[Std.random(t.getFrameLabels().length)];
-			tileSymbols.push(t);
-
-			trace(tileSymbols.length);
-		}
-	}
+        addChild(s);
+        s.loopMode = LoopMode.LOOP;
+        spriteSymbols.push(s);
+    }
 }
