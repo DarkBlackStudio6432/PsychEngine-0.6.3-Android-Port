f8fc1e41210c81867bbc16ce2403f57a91faef68
DarkBlackStudio6432 <95198141+DarkBlackStudio6432@users.noreply.github.com>
2026-01-30 22:18:59 -0300

    Atualizar o MusicBeatSubstate.hx

--- source/MusicBeatSubstate.hx
+++ source/MusicBeatSubstate.hx
@@ -2,10 +2,19 @@ package;
 
 import flixel.FlxSubState;
 import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.text.FlxText;
 
-import backend.ClientPrefs;
 import backend.Paths;
+import backend.ClientPrefs;
 import backend.Conductor;
+import backend.Highscore;
+
+#if mobile
+import mobile.objects.MobilePad;
+import mobile.backend.StorageUtil;
+import mobile.backend.PsychJNI;
+#end
 
 class MusicBeatSubstate extends FlxSubState
 {
