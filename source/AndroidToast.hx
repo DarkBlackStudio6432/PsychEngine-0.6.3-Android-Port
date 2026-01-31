1f91c3c43dd2928b4287e3dd5c93b89b0b515281
DarkBlackStudio6432 <95198141+DarkBlackStudio6432@users.noreply.github.com>
2026-01-31 19:54:27 -0300

    Atualizar o AndroidToast.hx

--- source/AndroidToast.hx
+++ source/AndroidToast.hx
@@ -4,7 +4,7 @@ import cpp.Lib;
 class AndroidToast {
     public static function show(message:String):Void {
         #if android
-        Lib.load("android", "showToast", CUZINHOOOOOO);
+        Lib.load("android", "showToast", "CUZINHOOOOOO");
         #end
     }
 }
