9c341aaed4898f83aef93ba389a62d58468f0656
DarkBlackStudio6432 <95198141+DarkBlackStudio6432@users.noreply.github.com>
2026-02-01 16:53:24 -0300

    Criar StorySubMenu.hx

---
+++ source/states/StorySubMenu.hx
@@ -0,0 +1,11 @@
+package states;
+
+import flixel.FlxSprite;
+import flixel.FlxState;
+
+class StorySubMenu extends FlxSprite {
+    public function new() {
+        super();
+        // stub vazio, nada dentro
+    }
+}
